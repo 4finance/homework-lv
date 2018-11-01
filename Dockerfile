1e9e88742144777ed3b5668d4253761a67ea17f0
shurikVish <shurik.vish@gmail.com>
2018-11-01 22:32:24 +0200

    project setup

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM gradle:jdk10 as build
+
+COPY --chown=gradle:gradle . /home/gradle/src
+WORKDIR /home/gradle/src
+RUN gradle build
+
+FROM openjdk:10-jre-slim
+EXPOSE 8080
+
+COPY --from=build /home/gradle/src/build/libs/*.jar homework.jar
+ENTRYPOINT exec java -jar /homework.jar
--- src/main/java/lv/finance/homework/HomeworkApplication.java
+++ src/main/java/lv/finance/homework/HomeworkApplication.java
@@ -1,4 +1,4 @@
-package io.fourfinanceit;
+package lv.finance.homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HomeworkApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HomeworkApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkApplication.class, args);
+	}
 }
