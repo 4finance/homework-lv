1c70d61e12eaf5681c92027943c2e3e9cd49fb37
shurikVish <shurik.vish@gmail.com>
2018-11-04 18:31:01 +0200

    api defined

--- src/main/java/lv/finance/homework/controller/LoanController.java
+++ src/main/java/lv/finance/homework/controller/LoanController.java
@@ -1,14 +1,28 @@
 package lv.finance.homework.controller;
 
+import lv.finance.homework.model.Loan;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class LoanController {
 
-	@GetMapping("/")
-	public String home() {
-		return "Hello 4finance";
+
+	@GetMapping("/loans")
+	public ResponseEntity<List<Loan>> getLoans(Long customerId) {
+		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping("/loan")
+	public ResponseEntity<?> createLoan() {
+		return ResponseEntity.badRequest().build();
+	}
+
+	@PostMapping("/loans/{loanId}/extend")
+	public ResponseEntity<?> extendLoan(@PathVariable Long loanId) {
+		return ResponseEntity.badRequest().build();
 	}
 }
