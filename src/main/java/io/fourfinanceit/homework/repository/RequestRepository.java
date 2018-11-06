935773866cd8bb95b28024577b87aacf518a4bc1
Alexander Vishnyakov <Alexander_Vishnyakov@rcomext.com>
2018-11-06 14:02:25 +0200

    request removed

--- src/main/java/io/fourfinanceit/homework/repository/RequestRepository.java
+++
@@ -1,9 +0,0 @@
-package io.fourfinanceit.homework.repository;
-
-import io.fourfinanceit.homework.model.Request;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RequestRepository extends JpaRepository<Request, Long> {
-}
