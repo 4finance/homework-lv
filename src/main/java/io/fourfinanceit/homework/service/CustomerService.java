c18dea8ae6469d74cae7efbfdb0087c6c4c244f0
Alexander Vishnyakov <Alexander_Vishnyakov@rcomext.com>
2018-11-06 14:04:57 +0200

    customer service

---
+++ src/main/java/io/fourfinanceit/homework/service/CustomerService.java
@@ -0,0 +1,28 @@
+package io.fourfinanceit.homework.service;
+
+import io.fourfinanceit.homework.controller.dto.CustomerDto;
+import io.fourfinanceit.homework.controller.dto.LoanDto;
+import io.fourfinanceit.homework.controller.dto.TermDto;
+import io.fourfinanceit.homework.model.Loan;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+    public void createCustomer(CustomerDto customer) {
+
+    }
+
+    public List<Loan> findLoans(String customerId) {
+        return null;
+    }
+
+    public void createLoan(String customerId, LoanDto loan) {
+
+    }
+
+    public void extendLoan(String customerId, String loanId, TermDto term) {
+
+    }
+}
