55d51f6e3e8723ba11ebc442f7fc14c2a8438611
Alexander Vishnyakov <Alexander_Vishnyakov@rcomext.com>
2018-11-06 15:55:30 +0200

    loan extension service

---
+++ src/main/java/io/fourfinanceit/homework/service/LoanExtensionService.java
@@ -0,0 +1,25 @@
+package io.fourfinanceit.homework.service;
+
+import io.fourfinanceit.homework.model.entity.Loan;
+import io.fourfinanceit.homework.model.entity.LoanExtension;
+import io.fourfinanceit.homework.repository.ExtensionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class LoanExtensionService {
+
+	private final ExtensionRepository extensionRepository;
+
+	@Autowired
+	public LoanExtensionService(ExtensionRepository extensionRepository) {
+		this.extensionRepository = extensionRepository;
+	}
+
+	public void extendLoan(Loan loan, Date term) {
+		LoanExtension extension = LoanExtension.builder().loan(loan).term(term).build();
+		extensionRepository.saveAndFlush(extension);
+	}
+}
