362682f3df0e7114f20b771e863e72f8b12130ff
Alexander Vishnyakov <Alexander_Vishnyakov@rcomext.com>
2018-11-06 15:54:54 +0200

    ResourceNotFoundException

---
+++ src/main/java/io/fourfinanceit/homework/service/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package io.fourfinanceit.homework.service.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+}
